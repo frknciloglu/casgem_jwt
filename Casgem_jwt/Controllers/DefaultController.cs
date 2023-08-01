91e7f6819c56602030ba8ec367b26a2959930ea1
frknciloglu <59924846+frknciloglu@users.noreply.github.com>
2023-08-01 14:43:03 +0300

    Add project files.

---
+++ Casgem_jwt/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem_jwt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[Action]")]
+        public IActionResult CustomerList()
+        {
+            return Ok("müşteri listesine eriştiniz");
+        }
+    }
+}
